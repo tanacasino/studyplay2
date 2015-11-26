ad3c51c9076f60066dec5c85e8f7a17de7f62a93
Tomofumi Tanaka <tanacasino@gmail.com>
2015-11-26 10:04:37 +0900

    Add insert sample code

--- app/services/UserService.scala
+++ app/services/UserService.scala
@@ -5,7 +5,7 @@ import javax.inject.Inject
 import scala.concurrent.{ ExecutionContext, Future }
 
 import models.User
-import services.repositories.Tables._; import profile.api._
+import services.repositories.Tables._, profile.api._
 
 class UserService @Inject() (implicit val ec: ExecutionContext) {
 
@@ -16,6 +16,35 @@ class UserService @Inject() (implicit val ec: ExecutionContext) {
       into ((u, userId) => UsersRow(userId, u._1, u._2, u._3))
     ) += ((name, password, isAdmin))
   }.map(toUser)
+
+
+  def insertExamples(name: String, password: String, isAdmin: Boolean = false): DBIO[UsersRow] = {
+    // 例えばこんなイメージのスキーマに対して
+    // case class UsersRow(userId: Long, name: String, password: String, isAdmin: Boolean)
+
+    // PK auto increment を指定せずにinsertする
+    // DBIO[Int] (insert件数)が返却される
+    Users
+      .map(u => (u.name, u.password, u.isAdmin)) += ((name, password, isAdmin))
+
+
+    // DBIO[Long] (pk件数)が返却されるようにする
+    Users
+      .map(u => (u.name, u.password, u.isAdmin))
+      .returning(Users.map(_.userId)) += ((name, password, isAdmin))
+
+
+    // DBIO[UsersRow] が返却されるようにする
+    // 内部的には、DBサーバからはPKしか帰ってこないので、返ってきたPKと += で渡したタプルを使ってUsersRowを作る
+    Users
+      .map(u => (u.name, u.password, u.isAdmin))
+      .returning(Users.map(_.userId))
+      .into { (u, userId) =>
+        // u は、+= で渡す予定のname/password/isAdminのタプル
+        UsersRow(userId, u._1, u._2, u._3)
+      } += ((name, password, isAdmin))
+  }
+
 
   def list(): DBIO[Seq[User]] = {
     Users.result
@@ -36,7 +65,6 @@ class UserService @Inject() (implicit val ec: ExecutionContext) {
       .headOption
   }.map(toUserOpt)
 
-  lazy val toUserOpt = lift(toUser)
 
   private def toUser(user: UsersRow): User = {
     User(
@@ -45,6 +73,8 @@ class UserService @Inject() (implicit val ec: ExecutionContext) {
       isAdmin = user.isAdmin
     )
   }
+
+  lazy val toUserOpt = lift(toUser)
 
   private def lift[A, B](f: A => B): Option[A] => Option[B] = _ map f
 
